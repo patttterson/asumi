6d51e538e8982e2b6ed9bfd2cff04d52aae3ed1f
patttterson <49377371+patttterson@users.noreply.github.com>
2026-02-27 13:28:19 -0800

    simple home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,24 @@
 <script lang="ts">
 </script>
 
-<h1>asumi</h1>
+<div class="home">
+  <div style="display: flex; align-items: baseline; gap: 0.5rem;">
+    <h1 style="margin: 0;">asumi</h1>
+    <h3 style="margin: 0;"><a href="https://github.com/patttterson/asumi">(source)</a></h3>
+  </div>
+  <p>patty's personal link shortener</p>
+  <p class="tip">tip: add ?redirect=no to the URL to see the destination</p>
+</div>
+
+<style>
+  .home {
+    position: relative;
+  }
+
+  .tip {
+    position: absolute;
+    top: 100%;
+    left: 0;
+    margin: 0.5rem 0 0;
+  }
+</style>
