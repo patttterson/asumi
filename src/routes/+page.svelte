71dd13055d15224ce3291b320501944de994b73a
patty <49377371+patttterson@users.noreply.github.com>
2026-05-08 23:54:35 -0700

    only show source on m.l and localhost

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+  const showSource = typeof window !== 'undefined' && ['minji.love', 'localhost'].includes(window.location.hostname);
 </script>
 
 <div class="home">
 	<div style="display: flex; align-items: baseline; gap: 0.5rem;">
 		<h1 style="margin: 0;">asumi</h1>
-		<h3 style="margin: 0;"><a href="https://github.com/patttterson/asumi">(source)</a></h3>
+		{#if showSource}
+		  <h3 style="margin: 0;"><a href="https://github.com/patttterson/asumi">(source)</a></h3>
+		{/if}
 	</div>
 	<p>patty's personal link shortener</p>
 	<p class="tip">tip: add ?redirect=no to the URL to see the destination</p>
